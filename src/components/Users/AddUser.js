18a801a653763d6e7061b945a847998c5712ecee
pawar-mahesh <pawar.mahesh2345@gmail.com>
2022-12-22 08:54:58 +0530

    src: AddUser: Fallow the naming convention for functions
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,13 +3,13 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const AddUser = (event) => {
+  const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
     <Card className={classes.input}>
-      <form onSubmit={AddUser}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text"></input>
         <label htmlFor="age">Age (Year)</label>
