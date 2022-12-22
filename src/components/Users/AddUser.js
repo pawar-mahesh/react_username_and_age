7818da84c9e576f8b195cb97787515bcf42c24dd
pawar-mahesh <pawar.mahesh2345@gmail.com>
2022-12-22 08:10:24 +0530

    src: compoents: Add Card component
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,21 @@
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
 const AddUser = (props) => {
   const AddUser = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={AddUser}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text"></input>
-      <label htmlFor="age">Age (Year)</label>
-      <input id="age" type="number"></input>
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={AddUser}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age (Year)</label>
+        <input id="age" type="number"></input>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
